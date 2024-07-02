0872dcae2d8519f7aa72382fcac62a0c0c8a7ce7
Yuvraj <devyuvraj883@gmail.com>
2024-07-03 00:44:37 +0530

    Added Menu and LogOut button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,21 @@
-
-import UserInfo from "./UserInfo";
+import { LogOut } from 'lucide-react'
+import MenuCommand from './MenuCommand'
+import UserInfo from './UserInfo'
 
 const MySidebar = () => {
   return (
-    <nav className="w-[300px] flex flex-col border-r-2 border-gray-400 min-h-screen p-2">
+    <nav className='w-[300px] flex flex-col border-r-2 border-gray-200 min-h-screen p-2'>
       <div>
-        <UserInfo/>
+        <UserInfo />
       </div>
-      <div className="grow">Menu</div>
-      <div>Settings</div>
+      <div className='grow py-2'>
+        <MenuCommand />
+      </div>
+      <div className='flex p-2 border-2 border-gray-200 cursor-pointer rounded-md gap-2 items-center justify-center'>
+        Log out <LogOut className='mr-2 h-4 w-4' />
+      </div>
     </nav>
-  );
+  )
 }
 
-export default MySidebar;
+export default MySidebar
