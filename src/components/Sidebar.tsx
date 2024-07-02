5402b39de6153734a61bb4164c3b6f2fafce8f1e
Yuvraj <devyuvraj883@gmail.com>
2024-07-02 23:43:07 +0530

    Added UserInfo component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,12 @@
-// components/MySidebar.js
+
+import UserInfo from "./UserInfo";
 
 const MySidebar = () => {
   return (
     <nav className="w-[300px] flex flex-col border-r-2 border-gray-400 min-h-screen p-2">
-      <div>User</div>
+      <div>
+        <UserInfo/>
+      </div>
       <div className="grow">Menu</div>
       <div>Settings</div>
     </nav>
